b2bef9c3904d2acfaab4a10af9ea8c7bb741c3f3
emarinfer <enriquemarin_sierra@hotmail.com>
2022-01-15 13:00:20 +0100

    modified the maximum zoom allowed

--- src/components/MapLayers.tsx
+++ src/components/MapLayers.tsx
@@ -4,6 +4,7 @@ export const MapLayers = () => (
 	<LayersControl position="topright">
 		<LayersControl.BaseLayer checked name="Mapa raster IGN - Spain">
 			<TileLayer
+				maxZoom={17}
 				url="https://tms-mapa-raster.ign.es/1.0.0/mapa-raster/{z}/{x}/{-y}.jpeg"
 				attribution='MTN ráster CC BY 4.0 <a href="http://www.ign.es/" target="_blank"><b>ign.es</b></a>'
 				crossOrigin={true}
@@ -25,6 +26,7 @@ export const MapLayers = () => (
 		</LayersControl.BaseLayer>
 		<LayersControl.BaseLayer name="OpenTopoMap">
 			<TileLayer
+				maxZoom={17}
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
 				crossOrigin={true}
